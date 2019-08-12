d3cf53392cd170c89c608e0fb99f08af1f5d3ba2
Nageo <Sarawut.st@gmail.com>
2019-08-13 01:35:35 +0700

    Drawn map 20190813

--- TDM/Models/sp_PROJECT_IMPACT_FindByColumn1_Result.cs
+++ TDM/Models/sp_PROJECT_IMPACT_FindByColumn1_Result.cs
@@ -10,18 +10,12 @@
 namespace TDM.Models
 {
     using System;
-    using System.Collections.Generic;
     
-    public partial class PROJECT_IMPACT
+    public partial class sp_PROJECT_IMPACT_FindByColumn1_Result
     {
         public int ID { get; set; }
         public string SUBJECT_ID { get; set; }
         public string SUBJECT_NAME { get; set; }
-        public string PROVINCE_ID { get; set; }
-        public string AMPHOE_ID { get; set; }
-        public string TAMBOL_ID { get; set; }
-        public string Area { get; set; }
-        public string Shape { get; set; }
         public System.DateTime CREATE_DATE { get; set; }
         public string CREATE_BY { get; set; }
         public System.DateTime UPDATE_DATE { get; set; }
@@ -29,6 +23,13 @@ namespace TDM.Models
         public Nullable<System.DateTime> PUBLISH_DATE { get; set; }
         public bool IS_PUBLISHED { get; set; }
         public bool IS_DELETED { get; set; }
+        public string PROVINCE_ID { get; set; }
+        public string ProvinceName { get; set; }
         public string Description { get; set; }
+        public string ShapeText { get; set; }
+        public string AMPHOE_ID { get; set; }
+        public string AmphoeName { get; set; }
+        public string TAMBOL_ID { get; set; }
+        public string TambolName { get; set; }
     }
 }
